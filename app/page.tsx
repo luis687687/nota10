6125c0cc1e20aca314107989c3256fbb27b09186
luis687687 <luismarque687@gmail.com>
2024-09-08 04:02:08 +0100

    sigin: updated code

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-export default function Page(){
-  return(
-    <div className="text-balance h-[400px] background-red color-red flexCenter ">
-      <span className="text-red">Testando o Tailwind!</span>
-    </div>
-  )
-}
